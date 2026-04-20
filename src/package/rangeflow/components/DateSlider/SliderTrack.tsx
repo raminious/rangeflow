a4969f9b19d8c1639ddc710d7a23e057bc30eb97
Ramin Mousavi <ramin.mousavy@gmail.com>
2026-04-20 19:45:31 +0100

    feat: keep selected date when extending range

--- src/package/rangeflow/components/DateSlider/SliderTrack.tsx
+++ src/package/rangeflow/components/DateSlider/SliderTrack.tsx
@@ -1,10 +1,13 @@
 import dayjs from 'dayjs'
 import { useMemo } from 'react'
 
-import { useRangeFlowStore } from '../../hooks/use-rangeflow-store'
+import { SLIDER_LEFT_SPACER, SLIDER_RIGHT_SPACER, SLIDER_THUMB } from '../../constants/slider'
 import { useDaysInRange } from '../../hooks/use-days-in-range'
+import { useRangeFlowRefs } from '../../hooks/use-rangeflow-refs'
+import { useRangeFlowStore } from '../../hooks/use-rangeflow-store'
 import { DoubleChevronLeftIcon } from '../../icons/DoubleChevronLeftIcon'
 import { DoubleChevronRightIcon } from '../../icons/DoubleChevronRightIcon'
+import { createSliderValues } from '../../utils/create-slider-values'
 import { DateTickers } from './DateTickers'
 import { RangeStepButton } from './RangeStepButton'
 import { Slider } from './Slider'
@@ -17,6 +20,9 @@ export function SliderTrack({ onHandleRef }: Props) {
   const update = useRangeFlowStore(state => state.update)
   const range = useRangeFlowStore(state => state.range)
   const daysInRange = useDaysInRange(range)
+  const {
+    slider: { root: rootRef }
+  } = useRangeFlowRefs()
 
   const disabledBefore = useRangeFlowStore(state => state.disabled?.before)
   const disabledAfter = useRangeFlowStore(state => state.disabled?.after)
@@ -31,6 +37,14 @@ export function SliderTrack({ onHandleRef }: Props) {
     [range.to, disabledAfter]
   )
 
+  const updateSlider = ({ left, right, size }: { left: number; right: number; size: number }) => {
+    rootRef.current?.setLayout({
+      [SLIDER_LEFT_SPACER]: left,
+      [SLIDER_THUMB]: size,
+      [SLIDER_RIGHT_SPACER]: right
+    })
+  }
+
   const handleMoveBackward = () => {
     update(draft => {
       const next = dayjs(draft.range.from).subtract(daysInRange / 2, 'day')
@@ -39,6 +53,10 @@ export function SliderTrack({ onHandleRef }: Props) {
         disabledBefore && next.isBefore(disabledBefore)
           ? dayjs(disabledBefore).toDate()
           : next.toDate()
+
+      draft.slider = createSliderValues(draft.range, draft.selected_date)
+
+      updateSlider(draft.slider)
     })
   }
 
@@ -48,6 +66,10 @@ export function SliderTrack({ onHandleRef }: Props) {
 
       draft.range.to =
         disabledAfter && next.isAfter(disabledAfter) ? dayjs(disabledAfter).toDate() : next.toDate()
+
+      draft.slider = createSliderValues(draft.range, draft.selected_date)
+
+      updateSlider(draft.slider)
     })
   }
 
